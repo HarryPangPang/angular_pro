81fc10921f624d61437c010dde59c34f779d21d1
Zhang <harry.zhang@edrington.com>
2018-12-10 18:10:45 +0800

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
 // import { from } from 'rxjs';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent,
+    SearchComponent,
+    ProductComponent,
+    StarsComponent,
   ],
   imports: [
     BrowserModule,
